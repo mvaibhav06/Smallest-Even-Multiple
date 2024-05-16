cb6231f967af09994333940ea31a979b6ead266e
mvaibhav06 <55031067+mvaibhav06@users.noreply.github.com>
2024-05-16 04:54:58 -0400

    Create SmallestEvenMultiple.java

---
+++ SmallestEvenMultiple.java
@@ -0,0 +1,9 @@
+class SmallestEvenMultiple {
+    public int smallestEvenMultiple(int n) {
+        if(n%2 == 0){
+            return n;
+        }else{
+            return 2*n;
+        }
+    }
+}
